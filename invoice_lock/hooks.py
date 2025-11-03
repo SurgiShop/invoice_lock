3d37e02a78fc022e277f5903dba84c2146e28016
garystarr-surgi <gary.starr@surgishop.com>
2025-11-03 14:32:11 -0500

    Update hooks.py

--- invoice_lock/hooks.py
+++ invoice_lock/hooks.py
@@ -1,1 +1,12 @@
+app_name = "invoice_lock"
+app_title = "Invoice Lock"
+app_publisher = "SurgiShop"
+app_description = "Locks customers with overdue invoices"
+app_email = "gary.starr@surgishop.com"
+app_license = "MIT"
 
+scheduler_events = {
+    "daily": [
+        "invoice_lock.overdue.check_overdue_invoices_and_lock_customers"
+    ]
+}
