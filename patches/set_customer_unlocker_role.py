44b023e9c7d7f9b8a99d10b95e0b74b1b81c88da
garystarr-surgi <gary.starr@surgishop.com>
2025-11-10 10:47:16 -0500

    Delete patches directory

--- patches/set_customer_unlocker_role.py
+++
@@ -1,9 +0,0 @@
-import frappe
-
-def execute():
-    user = "gary.starr@surgishop.com"
-    roles = frappe.get_roles(user)
-    if "Customer Unlocker" not in roles:
-        frappe.set_user_roles(user, roles + ["Customer Unlocker"])
-        frappe.db.commit()
-        frappe.logger().info(f"Assigned 'Customer Unlocker' to {user}")
